631e601e6ff5051463f7622e7569214c3b7d26e6
Ruth Foulis <ruthlilfoulis@gmail.com>
2021-01-29 11:50:08 +0000

    customer model

--- src/main/java/com/example/finalproject/models/Customer.java
+++ src/main/java/com/example/finalproject/models/Customer.java
@@ -1,4 +1,57 @@
 package com.example.finalproject.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name= "customers")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @JsonIgnoreProperties({"customer"})
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    private List<Order> orders;
+
+    public Customer(String name) {
+        this.name = name;
+        this.orders = new ArrayList<Order>();
+    }
+
+    public Customer(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
