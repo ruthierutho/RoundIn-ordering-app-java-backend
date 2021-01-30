aa283d4880c42099fa87b54128768bcc2833d62d
Mark <markmeldrum70@gmail.com>
2021-01-30 12:50:41 +0000

    Created order controller

--- src/main/java/com/example/finalproject/controllers/OrderController.java
+++ src/main/java/com/example/finalproject/controllers/OrderController.java
@@ -1,4 +1,41 @@
 package com.example.finalproject.controllers;
 
+import com.example.finalproject.models.Customer;
+import com.example.finalproject.models.Order;
+import com.example.finalproject.repositories.CustomerRepository;
+import com.example.finalproject.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class OrderController {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @GetMapping(value= "/orders")
+    public ResponseEntity<List<Order>>getAllOrdersAndFilters(
+            @RequestParam(required = false, name = "date") String date
+    ) {
+        if (date != null){
+            return new ResponseEntity<>(orderRepository.findAllByDate(date), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(orderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/orders/{id}")
+    public ResponseEntity getOrders(@PathVariable Long id) {
+        return new ResponseEntity<>(orderRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/orders")
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        orderRepository.save(order);
+        return new ResponseEntity<> (order, HttpStatus.CREATED);
+    }
 }
+
--- src/main/java/com/example/finalproject/repositories/OrderRepository.java
+++ src/main/java/com/example/finalproject/repositories/OrderRepository.java
@@ -3,5 +3,8 @@ package com.example.finalproject.repositories;
 import com.example.finalproject.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByDate(String date);
 }
