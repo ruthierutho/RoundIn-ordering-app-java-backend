bef4c10649d4039aaea4e0aa76a3db17d3c3bc53
Mark <markmeldrum70@gmail.com>
2021-01-30 12:36:09 +0000

    Added post request to customer controller

--- src/main/java/com/example/finalproject/controllers/CustomerController.java
+++ src/main/java/com/example/finalproject/controllers/CustomerController.java
@@ -5,10 +5,7 @@ import com.example.finalproject.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +28,11 @@ public class CustomerController {
     @GetMapping(value = "/customers/{id}")
     public ResponseEntity getCustomers(@PathVariable Long id) {
         return new ResponseEntity<>(customerRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/customers")
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        customerRepository.save(customer);
+        return new ResponseEntity<> (customer, HttpStatus.CREATED);
     }
 }
