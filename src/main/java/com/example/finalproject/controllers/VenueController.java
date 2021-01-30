14cea3459fc1e2782c215a160bfa0b55c84e081c
Chris Kite <chris.kat82@googlemail.com>
2021-01-30 13:43:45 +0000

    get request for venue controller

--- src/main/java/com/example/finalproject/controllers/VenueController.java
+++ src/main/java/com/example/finalproject/controllers/VenueController.java
@@ -1,4 +1,55 @@
 package com.example.finalproject.controllers;
 
+import com.example.finalproject.models.Customer;
+import com.example.finalproject.models.Venue;
+import com.example.finalproject.repositories.CustomerRepository;
+import com.example.finalproject.repositories.VenueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class VenueController {
+
+    @Autowired
+    VenueRepository venueRepository;
+
+    @GetMapping(value= "/venues")
+    public ResponseEntity<List<Venue>>getAllVenuesAndFilters(
+            @RequestParam(required = false, name = "name") String name
+    ){
+        if (name != null){
+            return new ResponseEntity<>(venueRepository.findAllByName(name), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(venueRepository.findAll(), HttpStatus.OK);
+    }
 }
+
+//
+//    @Autowired
+//    CustomerRepository customerRepository;
+//
+//    @GetMapping(value= "/customers")
+//    public ResponseEntity<List<Customer>>getAllCustomersAndFilters(
+//            @RequestParam(required = false, name = "name") String name
+//    ) {
+//        if (name != null){
+//            return new ResponseEntity<>(customerRepository.findAllByName(name), HttpStatus.OK);
+//        }
+//        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+//    }
+//
+//    @GetMapping(value = "/customers/{id}")
+//    public ResponseEntity getCustomers(@PathVariable Long id) {
+//        return new ResponseEntity<>(customerRepository.findById(id), HttpStatus.OK);
+//    }
+//
+//    @PostMapping(value = "/customers")
+//    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+//        customerRepository.save(customer);
+//        return new ResponseEntity<> (customer, HttpStatus.CREATED);
+//    }
+//}
