50f6acc115c48072e11ef3df3714ddfc5c27653c
Mark <markmeldrum70@gmail.com>
2021-01-30 12:24:49 +0000

    Created customer controller

--- src/main/java/com/example/finalproject/controllers/CustomerController.java
+++ src/main/java/com/example/finalproject/controllers/CustomerController.java
@@ -1,4 +1,35 @@
 package com.example.finalproject.controllers;
 
+import com.example.finalproject.models.Customer;
+import com.example.finalproject.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value= "/customers")
+    public ResponseEntity<List<Customer>>getAllCustomersAndFilters(
+            @RequestParam(required = false, name = "name") String name
+    ) {
+        if (name != null){
+            return new ResponseEntity<>(customerRepository.findAllByName(name), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+//    @GetMapping(value = "/customers/{id}")
+//    public ResponseEntity getCustomers(@PathVariable Long id) {
+//        return new ResponseEntity(customerRepository.findById(id), HttpStatus.OK);
+//    }
 }
--- src/main/java/com/example/finalproject/repositories/CustomerRepository.java
+++ src/main/java/com/example/finalproject/repositories/CustomerRepository.java
@@ -3,5 +3,8 @@ package com.example.finalproject.repositories;
 import com.example.finalproject.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findAllByName(String name);
 }
