c27d1e3ac50b1fd2418a2a3593b2457041a7e30e
Mark <markmeldrum70@gmail.com>
2021-01-30 14:10:01 +0000

    Customers added to data loader

--- src/main/java/com/example/finalproject/components/DataLoader.java
+++ src/main/java/com/example/finalproject/components/DataLoader.java
@@ -1,4 +1,45 @@
 package com.example.finalproject.components;
 
-public class DataLoader {
+import com.apple.eawt.Application;
+import com.example.finalproject.models.Customer;
+import com.example.finalproject.repositories.CustomerRepository;
+import com.example.finalproject.repositories.OrderRepository;
+import com.example.finalproject.repositories.VenueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    VenueRepository venueRepository;
+
+    public DataLoader(){
+
+    }
+
+    public void run(ApplicationArguments args){
+        Customer ruth = new Customer("Ruth");
+        customerRepository.save(ruth);
+
+        Customer neale = new Customer("Neale");
+        customerRepository.save(neale);
+
+        Customer chris = new Customer("Chris");
+        customerRepository.save(chris);
+
+        Customer mark = new Customer("Mark");
+        customerRepository.save(mark);
+    }
+
+
+
 }
