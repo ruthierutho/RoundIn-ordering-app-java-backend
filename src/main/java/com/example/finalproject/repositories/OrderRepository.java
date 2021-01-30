207f5334df57d73cece3735627490c489bbb24af
Chris Kite <chris.kat82@googlemail.com>
2021-01-30 11:34:17 +0000

    updated order repository to interface

--- src/main/java/com/example/finalproject/repositories/OrderRepository.java
+++ src/main/java/com/example/finalproject/repositories/OrderRepository.java
@@ -1,4 +1,7 @@
 package com.example.finalproject.repositories;
 
-public class OrderRepository {
+import com.example.finalproject.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
