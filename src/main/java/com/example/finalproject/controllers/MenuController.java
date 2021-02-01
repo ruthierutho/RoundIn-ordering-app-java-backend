ab6202329fdf9af66dd32528a9f8dedf4446c075
Mark <markmeldrum70@gmail.com>
2021-02-01 11:35:01 +0000

    Added menu controller and repo

--- src/main/java/com/example/finalproject/controllers/MenuController.java
+++ src/main/java/com/example/finalproject/controllers/MenuController.java
@@ -1,7 +1,56 @@
 package com.example.finalproject.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.finalproject.models.Menu;
+import com.example.finalproject.repositories.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class MenuController {
+    @Autowired
+    MenuRepository menuRepository;
+
+    @GetMapping(value= "/menus")
+    public ResponseEntity<List<Menu>> getAllMenusAndFilters(
+            @RequestParam(required = false, name = "name") String name
+    ) {
+        if (name != null){
+            return new ResponseEntity<>(menuRepository.findAllByName(name), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(menuRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/menus/{id}")
+    public ResponseEntity getMenu(@PathVariable Long id) {
+        return new ResponseEntity<>(menuRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/menus")
+    public ResponseEntity<Menu> createMenu(@RequestBody Menu menu) {
+        menuRepository.save(menu);
+        return new ResponseEntity<> (menu, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/menus/update/{id}")
+    public ResponseEntity updateMenu(
+            @PathVariable Long id,
+            @RequestBody String newName
+    ) {
+        Menu menuToUpdate = menuRepository.getOne(id);
+        if (newName != null) {
+            menuToUpdate.setName(newName);
+        }
+        return new ResponseEntity<>(menuRepository.getOne(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/menus/delete/{id}")
+    public ResponseEntity deleteMenu(@PathVariable Long id) {
+        Menu deletedMenu = menuRepository.getOne(id);
+        menuRepository.deleteById(id);
+        return new ResponseEntity<>(deletedMenu, HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/finalproject/repositories/MenuRepository.java
+++ src/main/java/com/example/finalproject/repositories/MenuRepository.java
@@ -1,7 +1,16 @@
 package com.example.finalproject.repositories;
 
+import com.example.finalproject.models.Drink;
+import com.example.finalproject.models.DrinkCategory;
 import com.example.finalproject.models.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    List<Menu> findAllByName(String name);
 }
